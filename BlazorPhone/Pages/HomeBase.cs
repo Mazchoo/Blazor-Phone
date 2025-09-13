c7559e8044024bbabb3d45e8a1d7b6643d2c3150
Matthew Mitchell <mazchoo@gmail.com>
2025-09-13 14:22:50 +0700

    Experiment: Used keypad grid for input

--- BlazorPhone/Pages/HomeBase.cs
+++ BlazorPhone/Pages/HomeBase.cs
@@ -4,9 +4,17 @@ namespace BlazorPhone.Pages
 {
     public class HomeBase : ComponentBase
     {
-        protected void HandleClick()
+        protected IEnumerable<string> Keys { get; set; } = new[]
         {
-            Console.WriteLine("Button clicked from HomeBase.cs!");
+            "1", "2", "3",
+            "4", "5", "6",
+            "7", "8", "9",
+            "*", "0", "#"
+        };
+
+        protected void HandleClick(string key)
+        {
+            Console.WriteLine($"Button {key} clicked");
         }
     }
 }
